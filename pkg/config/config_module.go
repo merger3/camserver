522c6de2fc8c6bcb1e0fc8a4bf113b3882670bdb
John Doe <johndoe@example.com>
2024-08-20 00:35:27 -0400

    Started swap config

--- pkg/config/config_module.go
+++ pkg/config/config_module.go
@@ -29,7 +29,7 @@ type Presets struct {
 }
 
 type CamPresets struct {
-	CamName string   `json:"camName"`
+	CamName string   `json:"name"`
 	Presets []string `json:"presets"`
 }
 
@@ -42,6 +42,11 @@ type PresetResponse struct {
 	CamPresetsList *CamPresets `json:"camPresets"`
 }
 
+type SwapResponse struct {
+	Found          bool        `json:"found"`
+	CamPresetsList *CamPresets `json:"camPresets"`
+}
+
 type ClickedCamRequest struct {
 	X           float64 `json:"x"`
 	Y           float64 `json:"y"`
@@ -50,23 +55,10 @@ type ClickedCamRequest struct {
 }
 
 func NewConfigManager() ConfigManager {
-	file, err := os.Open(filepath.Join("configs", "presets.json"))
-	if err != nil {
-		log.Fatalf("failed to open file: %s", err)
-	}
-	defer file.Close()
-
-	var p Presets
-
-	decoder := json.NewDecoder(file)
-	if err = decoder.Decode(&p); err != nil {
-		log.Fatalf("error unmarshalling JSON: %s", err)
-	}
-
-	return ConfigManager{p}
+	return ConfigManager{LoadPresets()}
 }
 
-func (c ConfigManager) LoadPresets() Presets {
+func LoadPresets() Presets {
 	file, err := os.Open(filepath.Join("configs", "presets.json"))
 	if err != nil {
 		log.Fatalf("failed to open file: %s", err)
@@ -91,10 +83,8 @@ func (c ConfigManager) GetPresets(ctx echo.Context) error {
 		return err
 	}
 
-	p := c.LoadPresets()
-
 	// This is slow, but p.Cameras should never be long enough for it to matter
-	for _, presets := range p.Cameras {
+	for _, presets := range c.Cameras {
 		if presets.CamName == req.Cam {
 			return ctx.JSON(http.StatusOK, PresetResponse{Found: true, CamPresetsList: &presets})
 		}
@@ -135,23 +125,66 @@ func (c ConfigManager) GetClickedCamConfig(ctx echo.Context, client *twitch.Clie
 		timeout = false
 		break
 	case <-time.After(10 * time.Second):
-		client.OnPrivateMessage(func(message twitch.PrivateMessage) {})
 		timeout = true
 		return ctx.NoContent(http.StatusOK)
 	}
+
+	client.OnPrivateMessage(func(message twitch.PrivateMessage) {})
 
 	if timeout {
 		return ctx.JSON(http.StatusOK, PresetResponse{Found: false, CamPresetsList: nil})
 	}
 
-	p := c.LoadPresets()
-
-	fmt.Println(cam)
-	for _, presets := range p.Cameras {
+	for _, presets := range c.Cameras {
 		if presets.CamName == cam {
 			return ctx.JSON(http.StatusOK, PresetResponse{Found: true, CamPresetsList: &presets})
 		}
 	}
 
 	return ctx.JSON(http.StatusOK, PresetResponse{Found: false, CamPresetsList: nil})
+}
+
+func (c ConfigManager) GetCamSwaps(ctx echo.Context, client *twitch.Client) error {
+	req := ClickedCamRequest{}
+
+	if err := ctx.Bind(&req); err != nil {
+		fmt.Printf("%v\n", err)
+		return err
+	}
+
+	ch := make(chan string)
+	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
+		if message.User.Name == "alveussanctuary" && len(strings.Fields(message.Message)) == 1 {
+			ch <- message.Message
+		}
+	})
+
+	scaleX := req.FrameWidth / videoWidth
+	scaleY := req.FrameHeight / videoHeight
+
+	x := req.X / scaleX
+	y := req.Y / scaleY
+
+	client.Say("alveusgg", fmt.Sprintf("!ptzgetcam %d %d", int(math.Round(x)), int(math.Round(y))))
+
+	var timeout bool
+	var cam string
+	select {
+	case v := <-ch:
+		fmt.Println(v)
+		cam = v
+		timeout = false
+		break
+	case <-time.After(10 * time.Second):
+		timeout = true
+		return ctx.NoContent(http.StatusOK)
+	}
+
+	client.OnPrivateMessage(func(message twitch.PrivateMessage) {})
+
+	if timeout {
+		return ctx.JSON(http.StatusOK, PresetResponse{Found: false, CamPresetsList: nil})
+	}
+
+	return ctx.JSON(http.StatusOK, PresetResponse{Found: true, Value: cam})
 }
