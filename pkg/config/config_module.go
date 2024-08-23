14387ba7fea3c53f76405befcf3a780c289a8f46
John Doe <johndoe@example.com>
2024-08-22 23:24:26 -0400

    Added giga long swaps definition

--- pkg/config/config_module.go
+++ pkg/config/config_module.go
@@ -144,47 +144,47 @@ func (c ConfigManager) GetClickedCamConfig(ctx echo.Context, client *twitch.Clie
 	return ctx.JSON(http.StatusOK, PresetResponse{Found: false, CamPresetsList: nil})
 }
 
-func (c ConfigManager) GetCamSwaps(ctx echo.Context, client *twitch.Client) error {
-	req := ClickedCamRequest{}
+// func (c ConfigManager) GetCamSwaps(ctx echo.Context, client *twitch.Client) error {
+// 	req := ClickedCamRequest{}
 
-	if err := ctx.Bind(&req); err != nil {
-		fmt.Printf("%v\n", err)
-		return err
-	}
+// 	if err := ctx.Bind(&req); err != nil {
+// 		fmt.Printf("%v\n", err)
+// 		return err
+// 	}
 
-	ch := make(chan string)
-	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-		if message.User.Name == "alveussanctuary" && len(strings.Fields(message.Message)) == 1 {
-			ch <- message.Message
-		}
-	})
+// 	ch := make(chan string)
+// 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
+// 		if message.User.Name == "alveussanctuary" && len(strings.Fields(message.Message)) == 1 {
+// 			ch <- message.Message
+// 		}
+// 	})
 
-	scaleX := req.FrameWidth / videoWidth
-	scaleY := req.FrameHeight / videoHeight
+// 	scaleX := req.FrameWidth / videoWidth
+// 	scaleY := req.FrameHeight / videoHeight
 
-	x := req.X / scaleX
-	y := req.Y / scaleY
+// 	x := req.X / scaleX
+// 	y := req.Y / scaleY
 
-	client.Say("alveusgg", fmt.Sprintf("!ptzgetcam %d %d", int(math.Round(x)), int(math.Round(y))))
+// 	client.Say("alveusgg", fmt.Sprintf("!ptzgetcam %d %d", int(math.Round(x)), int(math.Round(y))))
 
-	var timeout bool
-	var cam string
-	select {
-	case v := <-ch:
-		fmt.Println(v)
-		cam = v
-		timeout = false
-		break
-	case <-time.After(10 * time.Second):
-		timeout = true
-		return ctx.NoContent(http.StatusOK)
-	}
+// 	var timeout bool
+// 	var cam string
+// 	select {
+// 	case v := <-ch:
+// 		fmt.Println(v)
+// 		cam = v
+// 		timeout = false
+// 		break
+// 	case <-time.After(10 * time.Second):
+// 		timeout = true
+// 		return ctx.NoContent(http.StatusOK)
+// 	}
 
-	client.OnPrivateMessage(func(message twitch.PrivateMessage) {})
+// 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {})
 
-	if timeout {
-		return ctx.JSON(http.StatusOK, PresetResponse{Found: false, CamPresetsList: nil})
-	}
+// 	if timeout {
+// 		return ctx.JSON(http.StatusOK, PresetResponse{Found: false, CamPresetsList: nil})
+// 	}
 
-	return ctx.JSON(http.StatusOK, PresetResponse{Found: true, Value: cam})
-}
+// 	return ctx.JSON(http.StatusOK, PresetResponse{Found: true, Value: cam})
+// }
